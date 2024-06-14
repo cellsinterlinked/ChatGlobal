b7c0aa6fd98e2c32ef86aabdd3c762f6add2ddc4
Randy Hicks <randyh@qualtrics.com>
2024-06-14 12:51:19 -0600

    define and use interface types for the calendar component

--- Components/Calendar/Calendar.tsx
+++ Components/Calendar/Calendar.tsx
@@ -1,49 +1,36 @@
 import React from 'react';
-
 import CalendarDay from './CalendarDay';
 import createCalendar from '../Functions/CalendarCreate';
 
-interface CreatedCalendar {
-    days: Day[];
-    monthString: string;
-    year: number;
-}
-
-export interface Day {
-    name: string;
-    num: number;
-    status: any;
-}
-
-const item: CreatedCalendar = createCalendar(6, 2024);
+const item = createCalendar(6, 2024);
 
 const Calendar = () => {
-    return (
-        <div>
-            <div className="calendar_container">
-                <div className="calendar_body">
-                    <div className="calendar_title">
-                        <p>{item.monthString}</p>
-                        <p>{item.year}</p>
-                    </div>
-                    <div className="calendar_head">
-                        <div className="day_box">SUNDAY</div>
-                        <div className="day_box">MONDAY</div>
-                        <div className="day_box">TUESDAY</div>
-                        <div className="day_box">WEDNESDAY</div>
-                        <div className="day_box">THURSDAY</div>
-                        <div className="day_box">FRIDAY</div>
-                        <div className="day_box">SATURDAY</div>
-                    </div>
-                    <div className="calendar_content">
-                        {item.days.map((day, index) => {
-                            return <CalendarDay key={index} day={day} />;
-                        })}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div className="calendar_container">
+				<div className="calendar_body">
+					<div className="calendar_title">
+						<p>{item.monthString}</p>
+						<p>{item.year}</p>
+					</div>
+					<div className="calendar_head">
+						<div className="day_box">SUNDAY</div>
+						<div className="day_box">MONDAY</div>
+						<div className="day_box">TUESDAY</div>
+						<div className="day_box">WEDNESDAY</div>
+						<div className="day_box">THURSDAY</div>
+						<div className="day_box">FRIDAY</div>
+						<div className="day_box">SATURDAY</div>
+					</div>
+					<div className="calendar_content">
+						{item.days.map((day, index) => {
+							return <CalendarDay key={index} day={day} />;
+						})}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Calendar;
--- Components/Functions/CalendarCreate.tsx
+++ Components/Functions/CalendarCreate.tsx
@@ -1,70 +1,122 @@
-const createCalendar = (monthProp: number ,  yearProp: number) => {
-    
+import { Day } from '../types';
 
-
-
-    const monthStrings = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    const dayStrings = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-    const date = new Date(yearProp, monthProp, 1)
-    const monthString = monthStrings[date.getMonth()]
-    const month = date.getMonth()
-    const year = date.getFullYear()
-
- 
-    const altMonth = month + 1
-
-    const daysInMonth  = (month :number, year: number) => {
-        return new Date(year, month, 0).getDate()
-    }
-    
-    const firstDayOfMonth = (year: number, month:number ) => {
-        let datenew =  new Date(year, month, 1)
-        let dateIndex = datenew.getDay()
-        return dayStrings[dateIndex]
-    }
-
-
-    let content: object[] = []
-    let totalDays = daysInMonth(altMonth, year) + 1
-
-    let firstDay = firstDayOfMonth(year, month)
-    
-    if (firstDay === "Monday") {
-        content = [{name: "" ,num:0, status:"none" }, ...content]
-    } 
-    if (firstDay === "Tuesday") {
-        content = [{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" }, ...content]
-    } 
-    if (firstDay === "Wednesday") {
-        content = [{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" }, ...content]
-    }
-    if (firstDay === "Thursday") {
-        content = [{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" }, ...content]
-    }
-
-    if (firstDay === "Friday") {
-        content = [{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" }, ...content]
-    }
-
-    if (firstDay === "Saturday") {
-        content = [{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" },{name: "" ,num:0, status:"none" }, ...content]
-    }
-
-    for(let i = 1; i < totalDays; i++) {
-        content.push({name: "", num:i, status: true})
-    }
-
-    
-
-    const createdMonth = {
-        month: month,
-        year: year,
-        monthString: monthString,
-        days: content
-    }
-
-    return createdMonth
+interface CreatedMonth {
+	month: number;
+	year: number;
+	monthString: string;
+	days: Day[];
 }
+
+const createCalendar = (monthProp: number, yearProp: number) => {
+	const monthStrings = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+	const dayStrings = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday',
+	];
+
+	const date = new Date(yearProp, monthProp, 1);
+	const monthString = monthStrings[date.getMonth()];
+	const month = date.getMonth();
+	const year = date.getFullYear();
+
+	const altMonth = month + 1;
+
+	const daysInMonth = (month: number, year: number) => {
+		return new Date(year, month, 0).getDate();
+	};
+
+	const firstDayOfMonth = (year: number, month: number) => {
+		let datenew = new Date(year, month, 1);
+		let dateIndex = datenew.getDay();
+		return dayStrings[dateIndex];
+	};
+
+	let content: Day[] = [];
+	let totalDays = daysInMonth(altMonth, year) + 1;
+
+	let firstDay = firstDayOfMonth(year, month);
+
+	if (firstDay === 'Monday') {
+		content = [{ name: '', num: 0, status: 'none' }, ...content];
+	}
+	if (firstDay === 'Tuesday') {
+		content = [
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			...content,
+		];
+	}
+	if (firstDay === 'Wednesday') {
+		content = [
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			...content,
+		];
+	}
+	if (firstDay === 'Thursday') {
+		content = [
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			...content,
+		];
+	}
+
+	if (firstDay === 'Friday') {
+		content = [
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			...content,
+		];
+	}
+
+	if (firstDay === 'Saturday') {
+		content = [
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			{ name: '', num: 0, status: 'none' },
+			...content,
+		];
+	}
+
+	for (let i = 1; i < totalDays; i++) {
+		content.push({ name: '', num: i, status: true });
+	}
+
+	const createdMonth: CreatedMonth = {
+		month: month,
+		year: year,
+		monthString: monthString,
+		days: content,
+	};
+
+	return createdMonth;
+};
 
 export default createCalendar;
